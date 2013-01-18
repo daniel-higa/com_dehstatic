ce390fbb27b80f40fa4c566fd722dfc7aba1166f
Daniel Higa <daniel-higa@bugme-not.com>
2013-01-18 09:15:44 -0300

    -just a quick layout monkey-coded

--- site/views/dehstatic/tmpl/show_folder.php
+++ site/views/dehstatic/tmpl/show_folder.php
@@ -1,7 +1,21 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+if (!defined('FACEBOOK_APP_ID')) define('FACEBOOK_APP_ID', '401244233301710');
+$document =& JFactory::getDocument();
+?>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?php echo FACEBOOK_APP_ID; ?>";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div style="margin-right: 30px; margin-top: 20px; margin-bottom: 20px;" >
 <?php
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
 
 $path = 'static/' . $this->root . '/' . $this->id . '/';
 $file =  $path .'1.txt';
@@ -13,8 +27,14 @@ if (file_exists( $file ) and file_exists($images)) {
     $fimage = fopen($images, 'r');
     $image =  fgets($fimage);
     echo '<img src="' . $path . $image . '" alt="'. $this->id .'" style="float: left; margin-right: 10px;"/>';
-    echo '<p style="text-align:justify; min-width: 50%;">' . file_get_contents($file) . '</p><br/>';
+    echo '<p style="text-align:justify; min-width: 50%;">' . file_get_contents($file) . '</p>';
 }
 
+
+$document->addCustomTag('<meta property="fb:app_id" content="'. FACEBOOK_APP_ID .'"/>');
+
 ?>
+<div style="float: left;">
+    <div class="fb-comments" data-href="<?php echo JURI::current(); ?>" data-width="420" data-num-posts="10"></div>
+</div>
 </div>
